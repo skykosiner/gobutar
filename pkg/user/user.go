1cc9fe0ddd20ed9a2b6f1999b263eefdfb5edd0b
Sky Kosiner <sky@skykosiner.com>
2025-02-16 20:15:10 +0000

    feat: User add password stuff

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -15,8 +15,19 @@ type User struct {
 	Password string `sql:"password"`
 }
 
-func hashPassword(password string) string {
-	bcrypt.GenerateFromPassword()
+func hashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func validPassword(inputPassword, storedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(inputPassword))
+	return err == nil
+
 }
 
 func NewUser(db *sql.DB) http.HandlerFunc {
@@ -26,12 +37,23 @@ func NewUser(db *sql.DB) http.HandlerFunc {
 			Password string `json:"password"`
 		}
 
-		if err := json.NewDecoder(r.Body).Decode(newUserReq); err != nil {
+		if err := json.NewDecoder(r.Body).Decode(&newUserReq); err != nil {
 			slog.Error("Error getting new user json request.", "error", err, "r", r)
 			http.Error(w, "Error cerating user.", http.StatusBadRequest)
 			return
 		}
 
-		_, err := db.Exec("INSERT INTO users (name, password) VALUES (?,?)")
+		hashedPass, err := hashPassword(newUserReq.Password)
+		if err != nil {
+			slog.Error("Error hashing user password.", "error", err, "new user", newUserReq)
+			http.Error(w, "Error hashing your password. Please try again.", http.StatusBadRequest)
+			return
+		}
+
+		if _, err = db.Exec("INSERT INTO users (email, password) VALUES (?,?)", newUserReq.Email, hashedPass); err != nil {
+			slog.Error("Error creating new user.", "error", err, "new user", newUserReq)
+			http.Error(w, "Sorry there was an error please try again.", http.StatusBadRequest)
+			return
+		}
 	}
 }
